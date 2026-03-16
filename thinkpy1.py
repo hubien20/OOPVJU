fa65aca05ba4c5269fe590c219f219942f12c398
hubien <25112017@st.vju.ac.vn>
2026-03-17 03:46:37 +0700

    Enhance time and distance calculations in thinkpy1.py
    Added calculations for total minutes, hours, and pace metrics.

--- thinkpy1.py
+++ thinkpy1.py
@@ -1,5 +1,20 @@
 #time
-minutes = int(input('Nhập phút: '))
-seconds = int(input('Nhập giây: '))
+minutes = float(input('Nhập phút: '))
+seconds = float(input('Nhập giây: '))
 total_seconds = minutes * 60 + seconds
+total_minutes = minutes + seconds/60
+hours = total_seconds / 3600
 print("Total seconds:", total_seconds)
+
+#distance
+kilometers = float(input('Kilometers: '))
+miles = kilometers/1.61
+print("Miles: ",miles)
+
+# pace
+pace_minutes = total_minutes/miles
+pace_seconds = total_seconds/miles
+speed_mph = miles / hours
+print("Average pace: ", pace_minutes, "minutes per mile")
+print("Average pace: ",pace_seconds, "seconds per mile")
+print("Average speed:", speed_mph, "miles per hour")
