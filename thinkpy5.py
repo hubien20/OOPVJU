e5c75833610009e2b0ef59021c3ee32e35c8c3cf
hubien <25112017@st.vju.ac.vn>
2026-03-18 22:01:40 +0700

    Update thinkpy5.py

--- thinkpy5.py
+++ thinkpy5.py
@@ -19,3 +19,44 @@ class Dog:
         
 dog1 = Dog("Milu", "vàng", "Poodle", "vui")
 print(dog1.bark())
+
+class Car:
+    def __init__(self, brand, size, color, price):
+        self.brand=brand
+        self.size=size
+        self.color=color
+        self.price=price
+    def speed(self):
+        print("xe tăng tốc")
+    def brake(self):
+        print("xe giảm tốc")
+    def crash(self):
+        print("xe đã đâm")
+
+car = Car("bmw","sedan","black","3B")
+print(car.speed())
+
+class BankAccount:
+    def __init__(self, name, account_number, bank, balance):
+        self.name = name
+        self.account_number = account_number
+        self.bank = bank
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print("Đã gửi", amount)
+
+    def withdraw(self, amount):
+        if self.balance >= amount:
+            self.balance -= amount
+            print("Đã rút", amount)
+        else:
+            print("Không đủ tiền!")
+
+    def check_balance(self):
+        print("Số dư:", self.balance)
+
+
+account = BankAccount("bien", "123456789", "tcb", 987654321)
+account.withdraw(100000)
